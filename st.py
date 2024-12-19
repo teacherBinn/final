d415f6bd93c7fb4ce4aab656f6c9e345f0b51908
teacherBinn <qkrdbqls920@naver.com>
2024-12-19 15:04:55 +0900

    Update st.py

--- st.py
+++ st.py
@@ -4,11 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
 import matplotlib.font_manager as fm
 
-import platform
-from matplotlib import font_manager, rc
-plt.rcParams['axes.unicode_minus'] = False
-if platform.system() == 'Linux':
-    rc('font', family='NanumGothic')
+plt.rcParams['font.family'] = 'AppleGothic'
 
 # 페이지 기본 설정
 st.set_page_config(page_title="전력량 계산 및 가구별 전력량 시각화", layout="wide")
